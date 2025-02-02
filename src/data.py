347c3a738c0388a62d1435bcc3276f73d5d7ed35
Michael Santo Domingo <michaelsd.dev@gmail.com>
2025-02-02 17:36:08 +1100

    Adds function to download and validate multiple raw data files

--- src/data.py
+++ src/data.py
@@ -9,7 +9,7 @@ from tqdm import tqdm
 
 from src.paths import RAW_DATA_DIR
 
-def download_raw_data_file(year: int, month: int) -> Path:
+def download_one_file_of_raw_data(year: int, month: int) -> Path:
   """
   Downloads Parquet file with historical taxi rides for the given year and month
   """
@@ -38,3 +38,67 @@ def validate_raw_data(
   rides = rides[rides.pickup_datetime < next_month_start]
 
   return rides
+
+def load_raw_data(
+    year: int,
+    months: Optional[List[int]] = None
+) -> pd.DataFrame:
+    """
+    Loads raw data from local storage or downloads it from the NYC website, and
+    then loads it into a Pandas DataFrame
+
+    Args:
+        year: year of the data to download
+        months: months of the data to download. If `None`, download all months
+
+    Returns:
+        pd.DataFrame: DataFrame with the following columns:
+            - pickup_datetime: datetime of the pickup
+            - pickup_location_id: ID of the pickup location
+    """
+    rides = pd.DataFrame()
+
+    if months is None:
+        # download data for the entire year (all months)
+        months = list(range(1, 13))
+    elif isinstance(months, int):
+        # download data only for the month specified by the int `month`
+        months = [months]
+
+    for month in months:
+
+        local_file = RAW_DATA_DIR / f'rides_{year}-{month:02d}.parquet'
+        if not local_file.exists():
+            try:
+                # download the file from the NYC website
+                print(f'Downloading file {year}-{month:02d}')
+                download_one_file_of_raw_data(year, month)
+            except:
+                print(f'{year}-{month:02d} file is not available')
+                continue
+        else:
+            print(f'File {year}-{month:02d} was already in local storage')
+
+        # load the file into Pandas
+        rides_one_month = pd.read_parquet(local_file)
+
+        # rename columns
+        rides_one_month = rides_one_month[['tpep_pickup_datetime', 'PULocationID']]
+        rides_one_month.rename(columns={
+            'tpep_pickup_datetime': 'pickup_datetime',
+            'PULocationID': 'pickup_location_id',
+        }, inplace=True)
+
+        # validate the file
+        rides_one_month = validate_raw_data(rides_one_month, year, month)
+
+        # append to existing data
+        rides = pd.concat([rides, rides_one_month])
+
+    if rides.empty:
+        # no data, so we return an empty dataframe
+        return pd.DataFrame()
+    else:
+        # keep only time and origin of the ride
+        rides = rides[['pickup_datetime', 'pickup_location_id']]
+        return rides
